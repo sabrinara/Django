faa2cf1e5873e7511562d41bf23844bd0b1e54d8
sabrinara <you@example.com>
2023-12-06 11:52:14 +0600

    project_2_M-10.2

--- project_2/first_app/views.py
+++ project_2/first_app/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("this is home")
-    # return render('')
+    # return HttpResponse("this is home")
+    return render(request,'first_app/home.html')
