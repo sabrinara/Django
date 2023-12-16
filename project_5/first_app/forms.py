d06fe2ecd2528bc221054615e9da4cd979fbfaeb
sabrinara <you@example.com>
2023-12-16 22:53:14 +0600

    Multiple choicefield

--- project_5/first_app/forms.py
+++ project_5/first_app/forms.py
@@ -3,4 +3,13 @@ from django import forms
 class contactForm(forms.Form):
     name = forms.CharField(label = "User Name")
     email = forms.EmailField(label = "User Email")
- 
+    age  = forms.IntegerField()
+    weight = forms.FloatField()
+    balance = forms.DecimalField()
+    check = forms.BooleanField()
+    birthday = forms.DateField()
+    appointment = forms.DateTimeField()
+    CHOICES = [ ('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    size = forms.ChoiceField(choices=CHOICES)
+    MEAL = [ ('B', 'Beef'), ('P', 'Pepperoni'), ('M', 'Mashroom')]
+    pizza = forms.MultipleChoiceField(choices=MEAL)
