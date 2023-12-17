90fb3020e1e8ad46cf4da1ac3fc01195aaa2e10a
sabrinara <you@example.com>
2023-12-17 12:51:37 +0600

    chunk added for file

--- project_5/first_app/views.py
+++ project_5/first_app/views.py
@@ -19,7 +19,15 @@ def submit_form(request):
     return render(request, './first_app/form.html')
 
 def DjangoForm(request):
-    form = contactForm(request.POST)
-    if form.is_valid():
-        print(form.cleaned_data)
-    return render(request, './first_app/django_form.html',{'form': form})
+    if request.method == 'POST':
+        form = contactForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data['file']
+            with open('./first_app/upload/' + file.name, 'wb+') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+            print(form.cleaned_data)
+            return render(request, './first_app/django_form.html',{'form': form})
+    else:
+        form = contactForm()
+    return render(request, './first_app/django_form.html', {'form': form})
